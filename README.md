41827ea4fb013dc40d3e0434bb46c8fe0b206835
jiangdou2014 <381006295@qq.com>
2016-07-26 21:10:50 +0800

    init project

---
+++ README.md
@@ -0,0 +1,31 @@
+[全栈增长工程师实战](https://github.com/phodal/growth-in-action-python)源码
+===
+
+技术栈
+---
+
+ - Django
+ - Django REST Framework
+ - Django JWT 
+ - Selenium
+
+**Feature**
+
+ - Basic Blog Model
+ - Unit, Service, Function test
+ - RESTful API
+ - AutoComplete API
+ - Json Web Token Auth
+ - Sitemap
+
+Thanks to
+---
+
+Blogpost Model & Template basis on [https://www.djangorocks.com/tutorials/how-to-create-a-basic-blog-in-django/](https://www.djangorocks.com/tutorials/how-to-create-a-basic-blog-in-django/)
+
+License
+---
+
+© 2016 [Phodal Huang](https://www.phodal.com). This code is distributed under the MIT LICENSE in this directory.
+
+[待我代码编成，娶你为妻可好](http://www.xuntayizhan.com/person/ji-ke-ai-qing-zhi-er-shi-dai-wo-dai-ma-bian-cheng-qu-ni-wei-qi-ke-hao-wan/)
---
+++ blog/settings.py
@@ -0,0 +1,140 @@
+"""
+Django settings for blog project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.7/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+import datetime
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+##################
+# LOCAL SETTINGS #
+##################
+
+# Allow any settings to be defined in local_settings.py which should be
+# ignored in your version control system allowing for settings to be
+# defined per machine.
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+
+DEBUG = False
+
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'www.phodal.com'
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            '/Users/apple/PycharmProjects/untitled/templates/'
+        ],
+        'OPTIONS': {
+            'loaders': {
+                'django_mobile.loader.Loader',
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader'
+            },
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django_mobile.context_processors.flavour'
+            ],
+        },
+    },
+]
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    'django.contrib.sitemaps',
+    'django_comments',
+    'rest_framework',
+    'django_mobile',
+    'blogpost'
+)
+
+SITE_ID = 1
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    'django_mobile.middleware.MobileDetectionMiddleware',
+    'django_mobile.middleware.SetFlavourMiddleware'
+)
+
+ROOT_URLCONF = 'blog.urls'
+
+WSGI_APPLICATION = 'blog.wsgi.application'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+    }
+}
+# Internationalization
+# https://docs.djangoproject.com/en/1.7/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+    '/static/js/',
+    '/static/css/',
+)
+
+JWT_AUTH = {
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=14)
+}
+
+from blog.local_settings import *
