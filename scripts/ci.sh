b57a9226779cae92a84bca672e962ec2df4c2424
jiangdou2014 <381006295@qq.com>
2016-07-27 15:57:44 +0800

    five commit

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-virtualenv --distribute -p /usr/local/bin/python3.4 growth-django
-source growth-django/bin/activate
-pip3 install -r ../requirements.txt
-python3 ../manage.py test
-python3 ../manage.py test test
+#virtualenv --distribute -p /usr/local/bin/python3.4 growth-django
+#source growth-django/bin/activate
+pip3 install -r requirements.txt
+python3 manage.py test
+python3 manage.py test test
